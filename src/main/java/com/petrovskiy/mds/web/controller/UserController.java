913eee015abfafbdf6f872be7643365faa51be59
Yaraslau <gamasileusbed@gmail.com>
2022-10-19 19:26:45 +0300

    add KeySecurityConfig/keycloak

--- src/main/java/com/petrovskiy/mds/web/controller/UserController.java
+++ src/main/java/com/petrovskiy/mds/web/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.UUID;
 
