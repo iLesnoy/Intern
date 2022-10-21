40ac3c272dcc7fac81b56d3816a1c12da9d4433d
Yaraslau <gamasileusbed@gmail.com>
2022-10-21 10:49:19 +0300

    add user-api micro

---
+++ user-api/src/main/java/com/petrovskiy/mds/model/User.java
@@ -0,0 +1,62 @@
+package com.petrovskiy.mds.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Pattern(regexp = "^[\\p{Alpha}А-Яа-я]{2,36}$")
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Email
+    @Size(max = 32)
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    @Pattern(regexp = "^[\\p{Alpha}А-Яа-я]{2,36}$")
+    private String name;
+
+    @Enumerated
+    @Column(name = "role_id", nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updated;
+
+
+    @PrePersist
+    private void PrePersist(){
+        created = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        updated = created;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updated = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+    }
+
+}
