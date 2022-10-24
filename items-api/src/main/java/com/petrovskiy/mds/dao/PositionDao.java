a3d61cda4e3bc2f49bea7d02b310aa38547d72af
Yaraslau <gamasileusbed@gmail.com>
2022-10-24 08:17:32 +0300

    refactor (remove posit) from item-api

--- items-api/src/main/java/com/petrovskiy/mds/dao/PositionDao.java
+++
@@ -1,9 +0,0 @@
-package com.petrovskiy.mds.dao;
-
-import com.petrovskiy.mds.model.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface PositionDao extends JpaRepository<Position, BigInteger> {
-}
--- items-api/src/main/java/com/petrovskiy/mds/service/PositionService.java
+++
@@ -1,9 +0,0 @@
-package com.petrovskiy.mds.service;
-
-import com.petrovskiy.mds.service.BaseService;
-import com.petrovskiy.mds.service.dto.PositionDto;
-
-import java.math.BigInteger;
-
-public interface PositionService extends BaseService<PositionDto, BigInteger> {
-}
