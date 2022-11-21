523e63f4c4acd8052283e89a587b925bacd676ec
Yaraslau <gamasileusbed@gmail.com>
2022-11-21 23:19:26 +0300

    add integr/mock test

--- user-transaction-api/src/test/java/com/petrovskiy/mds/controller/UserTransactionControllerTest.java
+++ user-transaction-api/src/test/java/com/petrovskiy/mds/controller/UserTransactionControllerTest.java
@@ -1,6 +1,5 @@
 package com.petrovskiy.mds.controller;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.petrovskiy.mds.service.dto.CompanyDto;
 import com.petrovskiy.mds.service.dto.UserDto;
@@ -9,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -45,9 +43,39 @@ class UserTransactionControllerTest {
     void create() {
         wireMockServer.stubFor(post(urlEqualTo("/api/orders"))
                 .withHeader("Content-Type", equalTo("application/json"))
-                .withRequestBody(containing("\"id\": \" \""))
-                .withRequestBody(containing("\"email\": \"gf@gmail.com\""))
-                .withRequestBody(containing("\"name\": \"fdsgfd\""))
+                .withRequestBody(containing("\"id\": \"514fa2a6-7cfc-40e0-8ace-000578f0ab1d \""))
+                .withRequestBody(containing("\"amount\": \"200\""))
+                .withRequestBody(containing("\"user_id\": \"3087eee4-7d02-4e7d-b90f-1d21ba44ba8e\""))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+    }
+
+    @Test
+    void createTransaction() {
+        wireMockServer.stubFor(post(urlEqualTo("/api/createTransaction"))
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withRequestBody(containing("\"order\": \"{\"id\":\"4e029bdb-fbb3-4345-baa4-fe7541f5989b\"} \""))
+
+                .willReturn(aResponse()
+                        .withStatus(200)));
+    }
+
+    @Test
+    void createTransactionInvalidBody() {
+        wireMockServer.stubFor(post(urlEqualTo("/api/createTransaction"))
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withRequestBody(containing("\"order\": \"{\"id\":\"\"} \""))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+    }
+
+    @Test
+    void createWithInvalidParam() {
+        wireMockServer.stubFor(post(urlEqualTo("/api/orders"))
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withRequestBody(containing("\"id\": \"5fds \""))
+                .withRequestBody(containing("\"amount\": \"\""))
+                .withRequestBody(containing("\"user_id\": \"3087eee4-7d02-4e7d-b90f-1d21ba44ba8e\""))
                 .willReturn(aResponse()
                         .withStatus(200)));
     }
@@ -58,24 +86,9 @@ class UserTransactionControllerTest {
                 .withHeader("Content-Type", equalTo("application/json"))
                 .withRequestBody(containing("\"id\": \"privet\""))
                 .willReturn(aResponse()
-                        .withStatus(404)));
+                        .withStatus(500)));
     }
 
 
-    @Test
-    void findAll() {
-        wireMockServer.stubFor(
-                WireMock.get(WireMock.urlEqualTo("/api/orders"))
-                        .willReturn(aResponse()
-                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                                .withBodyFile("todo-api/response-200.json"))
-        );
-
-        this.webTestClient
-                .get()
-                .uri("/api/orders")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody().jsonPath("$.length()").isEqualTo(8);}
 
 }
