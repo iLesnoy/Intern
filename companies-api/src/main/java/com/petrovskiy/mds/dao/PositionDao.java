02cf8f283dc49b62f974d84760b654b88f0a7aec
Yaroslav Petrovskiy <87644515+iLesnoy@users.noreply.github.com>
2022-11-25 09:54:24 +0300

    Update PositionDao.java
    fix query

--- companies-api/src/main/java/com/petrovskiy/mds/dao/PositionDao.java
+++ companies-api/src/main/java/com/petrovskiy/mds/dao/PositionDao.java
@@ -2,7 +2,9 @@ package com.petrovskiy.mds.dao;
 
 import com.petrovskiy.mds.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -13,4 +15,8 @@ import java.util.Optional;
 public interface PositionDao extends JpaRepository<Position, BigInteger> {
 
     Optional<Position> findPositionsByIdAndAmountIsAfterOrAmountEquals(BigInteger id, BigDecimal amount,BigDecimal amount2);
+
+    @Modifying
+    @Query("update Position p set p.amount = p.amount - :amount where p.id = :id")
+    void updatePositionAmount(@Param("id") BigInteger positionId, @Param("amount") BigDecimal amount);
 }
